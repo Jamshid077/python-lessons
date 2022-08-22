6e7fc30c4e11218ae949659c4c954085cc25fad7
Jamshidbek077 <masharipovjorabek0@gmail.com>
2022-08-22 08:07:46 -0700

    Add to new folders
    python dasturlash tili boyicha takrorlash qildim

---
+++ python darslari.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# print('Hello World')
+# print('Hello \tworld')
+# print('hello \nworld')
+# print('salom'.upper())
+# print('Salom'.lower())
+# print('salom'.title())
+
+# sonlar=[1,4,5,7,-56,-87,45]
+# print(sonlar.sort())
+
+# List-07
+
+# mevalar=['olma','uzum','anjir','tarvuz']
+# print('Birinchi meeva:',mevalar[0].title())
+# print('Ikkinch meva:',mevalar[-1].upper())
+
+# Royxatga elementlarni qoshish
+# append() metodi
+
+#sonlar=[1,45,6,89,60,2,4,5]
+# sonlar.append(2)
+# print(sonlar)
+
+# insert() metodi 
+# sonlar.insert(0,'o\'nbir') 
+# print(sonlar)
+
+# del sonlar[4]
+# print(sonlar)
+
+# sonlar.remove(1)
+# print(sonlar)
+
+
+
+# mehmon=['Ali','jamshid','jo\'rabek']
+# for i in mehmon:
+    
+#     print(f"Salom {i.title()}, sizni nahorga oshga taklif qilamiz")
+
+
+# cars=['Audi','volvo','mersedes','nexia','damas','mallibu']
+# cars.sort()
+
+# cars.sort(reverse=True)
+
+# sorted(cars)
+
+# print(sorted(cars,reverse=True))
+
+# cars.reverse()
+# print(cars)
+
+# print(f"Toq_sonlar:{list(range(1,20,2))}")
+# print(f"Juft_sonlar:{list(range(0,20,2))}")
+# items=list(range(1,20,2))   
+# print(min(items)) 
+# print(max(items))
+# print(sum(items))
+
+# royxat kesib olish      
+      
+# print(items[0:3])
+      
+# print("5 ta eng yaqin dostingizni belgilang:")
+# dostlar=[]  
+# for i in range(5):
+#     dostlar.append(input(f"{i+1}-dostingizni belgilang:"))
+# print(dostlar)
+      
+# dostlar = [] # bo'sh ro'yxat
+# print("5 ta eng yaqin do'stingiz kim?")
+# for n in range(5): # n bu yerda 0 dan 4 gacha qiymatlar oladi
+#     dostlar.append(input(f"{n+1}-do'stingizning ismini kiriting: "))
+# print(dostlar)      
+      
+print('hello')
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ temp.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+a=int(input('asmoqwdnqowdsd\n'))
+print(3+2+a)
+
