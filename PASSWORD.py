5f67c99de4db788e899735cab13e9a387f8b4c97
Jamshidbek077 <masharipovjorabek0@gmail.com>
2022-08-26 08:07:36 -0700

    Create PASSWORD.py

---
+++ PASSWORD.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+# PASWORD GENERATOR PROGRAMM
+# import random
+# kichik_harflar='abcdefghijklmnopqrstuvwxyz'
+# katta_harflar='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# raqam_lar='0123456789'
+# symbl_lar='!@#$%^&*()_+=?/\|<>'
+
+# Use_for=kichik_harflar+katta_harflar+raqam_lar+symbl_lar
+# olchamlari=8
+
+# password="".join(random.sample(Use_for,olchamlari))
+# print("Sizning Password ingizni ochdim: "+password)
+# print('(ILTIMOS PAROL TOGRI BOLSA:HA/YOQ belgisini qoying)')
+# A=input("I KNOW1:")
+# if A=='ha':
+#     print('All right guys')
+# else:
+#     print("Sizning Password ingizni ochdim: "password)
+#     print('(ILTIMOS PAROL TOGRI BOLSA:HA/YOQ belgisini qoying)')
+#     A=input("I KNOW1:")
